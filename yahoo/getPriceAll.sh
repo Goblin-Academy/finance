a442ef51a7ab60f4c99fc56c679a6d06af9ef8d3
schaed <schae@cern.ch>
2017-01-02 23:41:02 -0500

    =add setup

---
+++ yahoo/getPriceAll.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /Users/schae/root_6_04/bin/thisroot.sh
+export PYTHONPATH=$PYTHONPATH:/Users/schae/testarea/finances/twilio-python
+python /Users/schae/testarea/finances/yahoo-finance/getPriceAll.py
